f55b396d70dca8a50c66bf1f54bc1135a9b8847d
vitoria174 <vitoriaalbuquerque174@gmail.com>
2025-06-24 21:34:46 -0300

    Finalizando CRUD do admin

--- modelo.py
+++ modelo.py
@@ -1,12 +1,16 @@
 import os
 import json
+import datetime
 
-arquivo_json = "arquivo.json"
+arquivo_json = "arquivo.json" #arquivo json que sera criado
+
+#classe onde o administrador criar, atualizara e deletarar os artigos.
 class Artigo():
       def __init__(self):
             self.artigo = arquivo_json
             self.dados = []
-            
+      
+      #metodo para ler o arquivo json
       def load(self):
             if os.path.exists(self.artigo):
                   with open(self.artigo, 'r') as file:
@@ -15,20 +19,55 @@ class Artigo():
                   self.dados = []
                   self.save()
       
+      #metodo para salvar artigos no json
       def save(self):
             with open(self.artigo, 'w') as file:
                   json.dump(self.dados, file, indent=4)
-                  
+      
+      #Função para criar artigos          
       def create(self):
+            titulo = str(input('Titulo: '))
+            descricao = str(input('Descricao: '))
+            
             dado = {
-                  'chave1' : 'valor1'
+                  'id':len(self.dados)+1,
+                  'Titulo':titulo,
+                  'Descricao':descricao,
+                  'Data':str(datetime.date.today())
             }
+            
             self.dados.append(dado)
             self.save()
+      
+      #Leitura dos artigos  
+      def read(self):
+            for i in self.dados:
+                  print(i)
+                  
+      #metodo de atualizacao dos artigos
+      def update(self):
+            update_id = int(input('Digite o id que deseja atualizar: '))
+            
+            for indice, chave in enumerate(self.dados):
+                  if chave['id'] == update_id:
+                        chave['Titulo'] = str(input('Titulo novo: '))
+                        chave['Descricao'] = str(input('Descricao: '))
+                        chave['Data'] = str(datetime.date.today())
+                        self.save()
+                        
+      def delete(self):
+            delete_id = int(input('Id que deseja deletar: '))
+            
+            for indice, id_chave in enumerate(self.dados):
+                  if delete_id == id_chave['id']:
+                       del self.dados[indice]
+                       self.save()
                         
 a = Artigo()
 a.load()
 a.create()
 a.create()
-a.create()
-a.load()
+a.read()
+a.update()
+a.read()
+a.delete()
